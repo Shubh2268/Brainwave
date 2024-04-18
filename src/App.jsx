44c3fd74ae951a627cc4c0e461001734221f37bb
Shubh <shubh2268@gmail.com>
2024-04-18 17:34:30 +0530

    --

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import ButtonGradient from "./assets/svg/ButtonGradient";
+import Button from './components/Button';
 
 function App() {
 
   return (
-    <div className='text-3xl font-bold underline'>
-      Hello Shubh!
-    </div>
+    <>
+      <div className='pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden'>
+        <Button className='mt-10' href='#login'>
+          Something
+        </Button>
+      </div>
+      <ButtonGradient />
+    </>
   )
 }
 
