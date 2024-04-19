379ea8108980d49b0cb7d473a62b18bfe5cadbef
Shubh <shubh2268@gmail.com>
2024-04-19 13:49:03 +0530

    --

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,6 +7,12 @@ const Header = () => {
                 <a className="block w-[12rem] xl:mr-8" href="#hero">
                     <img src={brainwave} alt="Brainwave" width={190} height={40} />
                 </a>
+
+                <nav className="hidden fixed top-[5rem] left-0 right-0 bottom-0 bg-n-8 lg:static lg:flex lg:mx-auto lg:bg-transparent">
+                    <div className="relative z-2 flex flex-col items-center justify-center m-auto lg:flex-row">
+                        
+                    </div>
+                </nav>
             </div>
         </div>
     )
