34e5b2ee259d5383ac14e4873c139ef922bfaeff
Shubh <shubh2268@gmail.com>
2024-04-23 17:46:17 +0530

    --

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ButtonGradient from "./assets/svg/ButtonGradient";
 import Header from './components/Header';
+import Section from './components/Section';
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
     <>
       <div className='pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden'>
         <Header />
+        <Section />
       </div>
       <ButtonGradient />
     </>
