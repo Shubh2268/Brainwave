690def499b1ef8841a38ab7f692fdeb25a2fe774
Shubh <shubh2268@gmail.com>
2024-05-07 17:39:26 +0530

    --

--- src/components/Generating.jsx
+++ src/components/Generating.jsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import { loading } from "../assets";
 
 const Generating = () => {
   return (
     <div>
-      
+      <img src={loading} alt='Loading' className="w-5 h-5 mr-4" />
+      AI is generating 
     </div>
   )
 }
