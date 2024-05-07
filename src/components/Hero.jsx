9c9c4104c01a72bab6955cbc599af723d9592dde
Shubh <shubh2268@gmail.com>
2024-05-07 17:26:35 +0530

    --

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -54,6 +54,8 @@ const Hero = () => {
                     <div className='absolute -top-[54%] left-1/2 w-[234%] -translate-x-1/2 md:-top-[46%] md:w-[138%] lg:-top-[104%]'>
                         <img src={heroBackground} alt='hero' className='w-full' width={1440} height={1800} />
                     </div>
+
+                    <BackgroundCircles />
                 </div>
             </div>
         </Section>
