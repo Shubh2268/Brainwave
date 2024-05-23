b425845990aaba827865e291b3c668bd9f7e3806
Shubh <shubh2268@gmail.com>
2024-05-23 17:44:22 +0530

    --

--- src/components/Benefits.jsx
+++ src/components/Benefits.jsx
@@ -10,13 +10,17 @@ const Benefits = () => {
 
         <div className='flex flex-wrap gap-10 mb-10'>
           {benefits.map((item) => (
-            <div key={item.id} className='block relative p-0.5 bg-no-repeat bg-[length:100%_100%] md:max-w-[24rem]' style={{backgroundImage: `url(${item.backgroundUrl})`}}>
+            <div key={item.id} className='block relative p-0.5 bg-no-repeat bg-[length:100%_100%] md:max-w-[24rem]' style={{ backgroundImage: `url(${item.backgroundUrl})` }}>
               <div className='relative z-2 flex flex-col min-h-[22rem] p-[2.4rem] pointer-events-none'>
                 <h5 className='h5 mb-5'>{item.title}</h5>
+
                 <p className='body-2 mb-6 text-n-3'>{item.text}</p>
+
                 <div className='flex items-center mt-auto'>
                   <img src={item.iconUrl} alt={item.title} width={48} height={48} />
+                  <p className='ml-auto font-code text-xs font-bold text-n-1 uppercase tracking-wider'>Explore more</p>
                 </div>
+                
               </div>
             </div>
           ))}
