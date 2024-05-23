648f6f1d913dcc25153004088e216415603ff04f
Shubh <shubh2268@gmail.com>
2024-05-23 17:13:59 +0530

    --

--- src/components/Benefits.jsx
+++ src/components/Benefits.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import { benefits } from '../constants'
 import Section from './Section'
 import Heading from './Heading'
 
@@ -7,6 +7,14 @@ const Benefits = () => {
     <Section id='features'>
       <div className='container relative z-2'>
         <Heading className='md:max-w-md lg:max-w-2xl' title='Chat Smarter, Not Harder with Brainwave' />
+
+        <div className='flex flex-wrap gap-10 mb-10'>
+          {benefits.map((item) => (
+            <div className='block relative p-0.5 bg-no-repeat bg-[length:100%_100%] md:max-w-[24rem]'>
+              
+            </div>
+          ))}
+        </div>
       </div>
     </Section>
   )
