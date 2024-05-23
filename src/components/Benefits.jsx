8194e2da1e4a769e20cfecb8c382d6da5c95727e
Shubh <shubh2268@gmail.com>
2024-05-23 16:53:11 +0530

    --

--- src/components/Benefits.jsx
+++ src/components/Benefits.jsx
@@ -4,7 +4,9 @@ import Section from './Section'
 const Benefits = () => {
   return (
     <Section id='features'>
-      
+      <div className='container relative z-2'>
+        
+      </div>
     </Section>
   )
 }
