f8dd7bf7711b3bdae0d534100bfa03e3a4160e0a
Shubh <shubh2268@gmail.com>
2024-05-24 16:59:44 +0530

    --

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import ButtonGradient from "./assets/svg/ButtonGradient";
 import Header from './components/Header';
 import Hero from './components/Hero';
 import Benefits from './components/Benefits';
+import Collaboration from './components/Collaboration';
 
 function App() {
 
@@ -12,6 +13,7 @@ function App() {
         <Header />
         <Hero />
         <Benefits />
+        <Collaboration />
       </div>
       <ButtonGradient />
     </>
