2b65a3cb917633c270e9a2fa69c6254ebaf1c719
Shubh <shubh2268@gmail.com>
2024-05-24 18:06:07 +0530

    --

--- src/components/Collaboration.jsx
+++ src/components/Collaboration.jsx
@@ -42,7 +42,9 @@ const Collaboration = () => {
                         <ul>
                             {collabApps.map((app, index) => (
                                 <li key={app.id} className={`absolute top-0 left-1/2 h-1/2 -ml-[1.6rem] origin-bottom rotate-${index * 45}`}>
-                                    
+                                    <div className={`relative -top-[1.6rem] flex w-[3.2rem] h-[3.2rem] bg-n-7 border border-n-1/15 rounded-xl -rotate-${index * 45}`}>
+                                        
+                                    </div>
                                 </li>
                             ))}
                         </ul>
