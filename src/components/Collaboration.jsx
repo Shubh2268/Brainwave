2b8dc2b88242152cf0b54afd085008624a54b63b
Shubh <shubh2268@gmail.com>
2024-05-24 17:32:40 +0530

    --

--- src/components/Collaboration.jsx
+++ src/components/Collaboration.jsx
@@ -1,4 +1,6 @@
 import Section from './Section';
+import { collabContent } from '../constants';
+import { check } from '../assets';
 
 const Collaboration = () => {
     return (
@@ -6,6 +8,17 @@ const Collaboration = () => {
             <div className='container lg:flex'>
                 <div className='max-w-[25rem]'>
                     <h2 className='h2 mb-4 md:mb-8'>AI Chat App for seamless collaboration</h2>
+
+                    <ul className='max-w-[22rem] mb-10 md:mb-14'>
+                        {collabContent.map((item) => (
+                            <li key={item.id} className='mb-3 py-3'>
+                                <div className='flex items-center'>
+                                    <img src={check} alt="check" width={24} height={24} />
+                                    <h6 className='body-2 ml-5'>{item.title}</h6>
+                                </div>
+                            </li>
+                        ))}
+                    </ul>
                 </div>
             </div>
         </Section>
