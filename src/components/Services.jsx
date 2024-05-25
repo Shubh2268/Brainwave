27ecadf79b687ab7576b0ee63969025ff9f3c15c
Shubh <shubh2268@gmail.com>
2024-05-25 17:50:51 +0530

    --

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,5 +1,6 @@
 import Section from './Section';
 import Heading from './Heading';
+import { service1 } from '../assets';
 
 const Services = () => {
   return (
@@ -10,7 +11,7 @@ const Services = () => {
             <div className='relative'>
                 <div className='relative z-1 flex items-center h-[39rem] mb-5 p-8 border border-n-1/10 rounded-3xl overflow-hidden lg:p-20 xl:h-[46rem]'>
                     <div className='absolute top-0 left-0 w-full h-full pointer-events-none md:w-3/5 xl:w-auto'>
-                        
+                        <img src={service1} alt="Smartest AI" width={800} height={730} />
                     </div>
                 </div>
             </div>
