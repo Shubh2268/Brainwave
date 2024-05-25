4beda883367d5f9b0f2176c7c33a08490f5ea85f
Shubh <shubh2268@gmail.com>
2024-05-25 18:04:57 +0530

    --

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,6 +1,7 @@
 import Section from './Section';
 import Heading from './Heading';
-import { service1 } from '../assets';
+import { service1, service2, service3, check } from '../assets';
+import { brainwaveServices } from '../constants';
 
 const Services = () => {
   return (
@@ -17,6 +18,13 @@ const Services = () => {
                     <div className='relative z-1 max-w-[17rem] ml-auto'>
                         <h4 className='h4 mb-4'>Smartest AI</h4>
                         <p className='body-2 mb-[3rem] text-n-3'>Brainwave unlocks the potential of AI-powered applications</p>
+                        <ul className='body-2'>
+                            {brainwaveServices.map((item, index) => (
+                                <li key={index} className='flex items-start py-4 border-t border-n-6'>
+                                    <img src={check} alt="check" width={24} height={24} />
+                                </li>
+                            ))}
+                        </ul>
                     </div>
                 </div>
             </div>
