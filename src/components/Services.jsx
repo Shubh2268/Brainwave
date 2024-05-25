8f51bec465b1ebdc884bb22da41ac3928f28b996
Shubh <shubh2268@gmail.com>
2024-05-25 17:56:09 +0530

    --

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -13,6 +13,11 @@ const Services = () => {
                     <div className='absolute top-0 left-0 w-full h-full pointer-events-none md:w-3/5 xl:w-auto'>
                         <img src={service1} alt="Smartest AI" width={800} height={730} className='w-full h-full object-cover md:object-right'/>
                     </div>
+
+                    <div className='relative z-1 max-w-[17rem] ml-auto'>
+                        <h4 className='h4 mb-4'>Smartest AI</h4>
+                        <p className='body-2 mb-[3rem] text-n-3'>Brainwave unlocks the potential of AI-powered applications</p>
+                    </div>
                 </div>
             </div>
         </div>
