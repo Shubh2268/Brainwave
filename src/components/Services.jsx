09a350844acab74022bc646723e3a9ec0daae1cb
Shubh <shubh2268@gmail.com>
2024-05-25 17:52:06 +0530

    --

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -11,7 +11,7 @@ const Services = () => {
             <div className='relative'>
                 <div className='relative z-1 flex items-center h-[39rem] mb-5 p-8 border border-n-1/10 rounded-3xl overflow-hidden lg:p-20 xl:h-[46rem]'>
                     <div className='absolute top-0 left-0 w-full h-full pointer-events-none md:w-3/5 xl:w-auto'>
-                        <img src={service1} alt="Smartest AI" width={800} height={730} />
+                        <img src={service1} alt="Smartest AI" width={800} height={730} className='w-full h-full object-cover md:object-right'/>
                     </div>
                 </div>
             </div>
