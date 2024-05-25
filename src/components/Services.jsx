1e833790b8a6d49b929a5420aab721de82b19ba0
Shubh <shubh2268@gmail.com>
2024-05-25 20:38:56 +0530

    --

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -57,6 +57,16 @@ const Services = () => {
                                     The world's most powerful AI photo and video art generation
                                     engine. What will you create?
                                 </p>
+
+                                <ul className='flex items-center justify-between'>
+                                    {brainwaveServicesIcons.map((item, index) => (
+                                        <li key={index} className={`rounded-2xl flex items-center justify-center ${index === 2 ? 'w-[3rem] h-[3rem] p-0.25 bg-conic-gradient md:w-[4.5rem] md:h-[4.5rem]' : 'flex w-10 h-10 bg-n-6 md:w-15 md:h-15'}`}>
+                                            <div className={index === 2 ? 'flex items-center justify-center w-full h-full bg-n-7 rounded-[1rem]' : ''}>
+                                                <img src={item} alt={item} />
+                                            </div>
+                                        </li>
+                                    ))}
+                                </ul>
                             </div>
                         </div>
                     </div>
