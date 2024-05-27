274633ca41da0f6dee3f5f9e33bc0a324c9ab4b5
Shubh <shubh2268@gmail.com>
2024-05-27 17:57:25 +0530

    --

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Hero from './components/Hero';
 import Benefits from './components/Benefits';
 import Collaboration from './components/Collaboration';
 import Services from './components/Services';
+import Pricing from './components/Pricing';
 
 function App() {
 
@@ -16,6 +17,7 @@ function App() {
         <Benefits />
         <Collaboration />
         <Services />
+        <Pricing />
       </div>
       <ButtonGradient />
     </>
