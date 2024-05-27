2bfd05041ca2b18c92febf2e16507340604e027e
Shubh <shubh2268@gmail.com>
2024-05-27 18:09:52 +0530

    --

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -6,7 +6,7 @@ const Pricing = () => {
     <Section id='pricing' className='overflow-hidden'>
         <div className='container relative z-2'>
             <div className='hidden relative justify-center mb-[6.5rem] lg:flex'>
-                <img src={smallSphere} alt='Sphere' width={255} height={255} />
+                <img src={smallSphere} alt='Sphere' width={255} height={255} className='relative z-1' />
             </div>
         </div>
     </Section>
