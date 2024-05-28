3fa74a70541e02fb5e737b5e6629867122cda6d9
Shubh <shubh2268@gmail.com>
2024-05-28 17:42:41 +0530

    --

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -8,7 +8,7 @@ const Pricing = () => {
             <div className='hidden relative justify-center mb-[6.5rem] lg:flex'>
                 <img src={smallSphere} alt='Sphere' width={255} height={255} className='relative z-1' />
                 <div className='absolute top-1/2 left-1/2 w-[60rem] -translate-x-1/2 -translate-y-1/2 pointer-events-none'>
-                  <img src={stars} alt='Stars' width={950} height={400} />
+                  <img src={stars} alt='Stars' width={950} height={400} className='w-full' />
                 </div>
             </div>
         </div>
