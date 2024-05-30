e35789a7217b8fcc125d8b408fde21a47273d9b4
Shubh <shubh2268@gmail.com>
2024-05-30 17:50:05 +0530

    --

--- src/components/Roadmap.jsx
+++ src/components/Roadmap.jsx
@@ -1,10 +1,12 @@
-import React from 'react';
 import Section from './Section';
+import Heading from './Heading';
 
 const Roadmap = () => {
   return (
    <Section id='roadmap' className='overflow-hidden'>
-
+        <div className='container md:pb-10'>
+            <Heading tag='Ready to get started' title="What we're working on" />
+        </div>
    </Section>
   )
 }
