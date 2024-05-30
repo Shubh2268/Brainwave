97480995e55e68c10bb5f8a93b4fcfca8545f026
Shubh <shubh2268@gmail.com>
2024-05-30 17:54:28 +0530

    --

--- src/components/Roadmap.jsx
+++ src/components/Roadmap.jsx
@@ -1,11 +1,20 @@
 import Section from './Section';
 import Heading from './Heading';
+import { roadmap } from '../constants';
 
 const Roadmap = () => {
   return (
    <Section id='roadmap' className='overflow-hidden'>
         <div className='container md:pb-10'>
             <Heading tag='Ready to get started' title="What we're working on" />
+
+            <div className='relative grid gap-6 md:grid-cols-2 md:gap-4 md:pb-[7rem]'>
+                {roadmap.map((item) => {
+                    const status = item.status === "done" ? "Done" : "In progress";
+
+                    
+                })}
+            </div>
         </div>
    </Section>
   )
