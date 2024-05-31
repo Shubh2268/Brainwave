7fb650f061ad10b40bd47cb4c5bb0c9e16e8d9d2
Shubh <shubh2268@gmail.com>
2024-05-31 17:52:57 +0530

    --

--- src/components/Roadmap.jsx
+++ src/components/Roadmap.jsx
@@ -1,7 +1,7 @@
 import Section from './Section';
 import Heading from './Heading';
 import { roadmap } from '../constants';
-import { grid } from '../assets';
+import { check2 ,grid, loading1 } from '../assets';
 
 const Roadmap = () => {
     return (
@@ -18,6 +18,12 @@ const Roadmap = () => {
                                 <div className='relative p-8 bg-n-8 rounded-[2.4375rem] overflow-hidden xl:p-15'>
                                     <div className='absolute top-0 left-0 max-w-full'>
                                         <img src={grid} alt='Grid' width={550} height={550} className='w-full' />
+                                    </div>
+
+                                    <div className='relative z-1'>
+                                        <div className='flex items-center justify-between max-w-[27rem] mb-8 md:mb-20'>
+                                            
+                                        </div>
                                     </div>
                                 </div>
                             </div>
