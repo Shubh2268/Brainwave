46d280ebb6aa78539e3f10adce48d4d6326e1a3d
Shubh <shubh2268@gmail.com>
2024-05-31 18:11:42 +0530

    --

--- src/components/Roadmap.jsx
+++ src/components/Roadmap.jsx
@@ -32,7 +32,7 @@ const Roadmap = () => {
                                         </div>
 
                                         <div className='mb-10 -my-10 -mx-15'>
-                                            <img src={item.imageUrl} alt={item.title} width={628} height={426} />
+                                            <img src={item.imageUrl} alt={item.title} width={628} height={426} className='w-full' />
                                         </div>
                                     </div>
                                 </div>
