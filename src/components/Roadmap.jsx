9d708976824e79fc04a9c5dea342dfe95ad6a772
Shubh <shubh2268@gmail.com>
2024-05-31 17:39:57 +0530

    --

--- src/components/Roadmap.jsx
+++ src/components/Roadmap.jsx
@@ -3,25 +3,29 @@ import Heading from './Heading';
 import { roadmap } from '../constants';
 
 const Roadmap = () => {
-  return (
-   <Section id='roadmap' className='overflow-hidden'>
-        <div className='container md:pb-10'>
-            <Heading tag='Ready to get started' title="What we're working on" />
+    return (
+        <Section id='roadmap' className='overflow-hidden'>
+            <div className='container md:pb-10'>
+                <Heading tag='Ready to get started' title="What we're working on" />
 
-            <div className='relative grid gap-6 md:grid-cols-2 md:gap-4 md:pb-[7rem]'>
-                {roadmap.map((item) => {
-                    const status = item.status === "done" ? "Done" : "In progress";
+                <div className='relative grid gap-6 md:grid-cols-2 md:gap-4 md:pb-[7rem]'>
+                    {roadmap.map((item) => {
+                        const status = item.status === "done" ? "Done" : "In progress";
 
-                    return (
-                        <div className={`md:flex even:md:translate-y-[7rem] p-0.25 rounded-[2.5rem ${item.colorful ? 'bg-conic-gradient' : 'bg-n-6'}`}>
-                            
-                        </div>
-                    )
-                })}
+                        return (
+                            <div key={item.id} className={`md:flex even:md:translate-y-[7rem] p-0.25 rounded-[2.5rem ${item.colorful ? 'bg-conic-gradient' : 'bg-n-6'}`}>
+                                <div className='relative p-8 bg-n-8 rounded-[2.4375rem] overflow-hidden xl:p-15'>
+                                    <div className='absolute top-0 left-0 max-w-full'>
+                                    
+                                    </div>
+                                </div>
+                            </div>
+                        )
+                    })}
+                </div>
             </div>
-        </div>
-   </Section>
-  )
+        </Section>
+    )
 }
 
 export default Roadmap;
