00e1358d3a5a281b89b48ece29fbd7904b76fd58
Shubh <shubh2268@gmail.com>
2024-06-02 18:19:23 +0530

    --

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,6 +9,10 @@ const Footer = () => {
                     © {new Date().getFullYear()}. All rights reserved.
                 </p>
 
+                <p className='caption text-n-4 lg:block'>
+                    Developed by <span className='font-semibold'>SHUBHAM TIWARI</span>
+                </p>
+
                 <ul className='flex gap-5 flex-wrap'>
                     {socials.map((item) => (
                         <a href={item.url} target='_blank' key={item.id} className='flex items-center justify-center w-10 h-10 bg-n-7 rounded-full transition-colors hover:bg-n-6'>
